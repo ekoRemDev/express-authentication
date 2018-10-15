83a09365d9565a3c94aeabd36418eeb516d8003d
ekoremdev@gmail.com <ekoremdev@gmail.com>
2018-10-15 21:50:36 +0400

    page authorization controls

--- app.js
+++ app.js
@@ -17,21 +17,31 @@ app.use(upload.array());
 app.use(cookieParser());
 app.use(session({secret: "Your secret key"}));
 
-var Users = [
-    {userid: '1', password: '1'},
-    {userid: '2', password: '1'},
-    {userid: '3', password: '1'},
-    {userid: '4', password: '1'},
-    {userid: '5', password: '1'}
-
-];
+// var Users = [
+//     {userid: '1', password: '1'},
+//     {userid: '2', password: '1'},
+//     {userid: '3', password: '1'},
+//     {userid: '4', password: '1'},
+//     {userid: '5', password: '1'}
+//
+// ];
 // var userIds = [1,2,3,4,5];
 var userIds = [
     {userid: 1, password: '1'},
     {userid: 2, password: '2'},
     {userid: 3, password: '3'},
     {userid: 4, password: '4'},
-    {userid: 123123, password: '123123'}
+    {userid: 123123, password: '123123'},
+    {userid: 6, password: '6'},
+];
+
+var messages = [
+    {messageid: 1, messageUserId:1, message:'message 1 - 1'},
+    {messageid: 2, messageUserId:1, message:'message 1 - 2'},
+    {messageid: 3, messageUserId:2, message:'message 2 - 1'},
+    {messageid: 4, messageUserId:3, message:'message 3 - 1'},
+    {messageid: 5, messageUserId:4, message:'message 4 - 1'},
+    {messageid: 6, messageUserId:1123123, message:'message 123123 - 1'},
 ];
 
 
@@ -41,14 +51,11 @@ app.get('/', function(req, res){
 });
 
 app.get('/login', function (req, res) {
-    // if (!req.session.memberLoggedIn) {
-    //     res.render('login');
-    // } else {
-    //     res.redirect('/member');
-    // }
-
-
-    res.render('login');
+    if(req.session.memberLoggedIn){
+        res.redirect('/member');
+    }else{
+        res.render('login');
+    }
 });
 
 app.post('/login',(req,res)=>{
@@ -75,33 +82,64 @@ app.post('/login',(req,res)=>{
 });
 
 app.get('/signup', function(req, res){
-    res.render('signup');
+    if(req.session.memberLoggedIn){
+        res.redirect('/member');
+    }else{
+        res.render('signup');
+    }
 });
 
 
-// app.post('/signup', function(req, res){
-//     if(!req.body.id || !req.body.password){
-//         res.status("400");
-//         res.send("Invalid details!");
-//     } else {
-//         Users.filter(function(user){
-//             if(user.id === req.body.id){
-//                 res.render('signup', {
-//                     message: "User Already Exists! Login or choose another user id"});
-//             }
-//         });
-//         var newUser = {id: req.body.id, password: req.body.password};
-//         Users.push(newUser);
-//         req.session.user = newUser;
-//         res.redirect('/protected_page');
-//     }
-// });
+app.post('/signup', function(req, res){
+    // console.log(userIds);
+    // console.log('======================');
+    // console.log(req.body.id);
+    // console.log(req.body.password);
+
+    var found = userIds.find(function(element) {
+        if(element.userid === parseInt(req.body.id)){
+            return element.userid === parseInt(req.body.id);
+        }
+    });
+
+    if (found) {
+        console.log('There is already In Use');
+        res.redirect('/signup');
+    } else {
+        console.log('New Member');
+        var newUser = {userid: req.body.id, password: req.body.password};
+        userIds.push(newUser);
+        req.session.memberID = parseInt(req.body.id);
+        req.session.memberLoggedIn = 'loggedin';
+        res.redirect('/member');
+    }
+
+    // if(!req.body.id || !req.body.password){
+    //     res.status("400");
+    //     // res.send("Invalid details!");
+    // } else {
+    //     userIds.filter(function(user){
+    //         if(user.userid === req.body.id){
+    //             res.render('signup', {
+    //                 message: "User Already Exists! Login or choose another user id"});
+    //         };
+    //         console.log('already');
+    //     });
+    //     var newUser = {userid: req.body.id, password: req.body.password};
+    //     userIds.push(newUser);
+    //     req.session.user = newUser;
+    //     res.redirect('/member');
+    // }
+});
 
 
 
 
 app.get('/member',checkSignIn, function(req, res){
-    res.render('member');
+    res.render('member',{
+        userid : req.session.memberID
+    });
+
 });
 app.use('/member', function(err, req, res, next){
     console.log(err);
